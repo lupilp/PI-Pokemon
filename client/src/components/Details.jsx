0ce6df606a995ed868314e47ae027401b0eed70b
lupilp <lupilp97@gmail.com>
2022-10-23 00:23:32 -0300

    stats

--- client/src/components/Details.jsx
+++ client/src/components/Details.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { getDetail, getDetailFromState } from "../actions";
+import styles from "../styles/Details.module.css";
 
 function Details() {
   const dispatch = useDispatch();
@@ -28,9 +29,33 @@ function Details() {
           <img src={pokemonDetail[0].image} alt="imagen-del-pokemon" />
           <div>{pokemonDetail[0].id}</div>
           <div>{pokemonDetail[0].hp}</div>
+          <div className={styles.barra}>
+            <div
+              className={styles.barraPintada}
+              style={{ width: `${(pokemonDetail[0].hp / 125) * 100}%` }}
+            ></div>
+          </div>
           <div>{pokemonDetail[0].attack}</div>
+          <div className={styles.barra}>
+            <div
+              className={styles.barraPintada}
+              style={{ width: `${(pokemonDetail[0].attack / 125) * 100}%` }}
+            ></div>
+          </div>
           <div>{pokemonDetail[0].defense}</div>
+          <div className={styles.barra}>
+            <div
+              className={styles.barraPintada}
+              style={{ width: `${(pokemonDetail[0].defense / 125) * 100}%` }}
+            ></div>
+          </div>
           <div>{pokemonDetail[0].speed}</div>
+          <div className={styles.barra}>
+            <div
+              className={styles.barraPintada}
+              style={{ width: `${(pokemonDetail[0].speed / 125) * 100}%` }}
+            ></div>
+          </div>
           <div>{pokemonDetail[0].height}</div>
           <div>{pokemonDetail[0].weight}</div>
           <ul>
