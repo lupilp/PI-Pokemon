43788341a7b6e8be7b866bd62fbe940e6f1d506f
lupilp <lupilp97@gmail.com>
2022-10-24 12:36:53 -0300

    detailsFix

--- client/src/components/Details.jsx
+++ client/src/components/Details.jsx
@@ -34,94 +34,102 @@ function Details() {
       {pokemonDetail.length ? (
         <div className={styles.contGral}>
           <div className={styles.contRed}>
-            <div className={styles.contIzq}>
-              <div className={styles.circulo}>
-                <img
-                  src={pokemonDetail[0].image}
-                  alt="imagen-del-pokemon"
-                  className={styles.image}
-                />
-              </div>
-              <div className={styles.infoBasica}>
-                <div className={styles.name}>
-                  {pokemonDetail[0].name[0].toUpperCase() +
-                    pokemonDetail[0].name.slice(1)}
+            <div className={styles.contGris}>
+              <div className={styles.contIzq}>
+                <div className={styles.circulo}>
+                  <img
+                    src={pokemonDetail[0].image}
+                    alt="imagen-del-pokemon"
+                    className={styles.image}
+                  />
                 </div>
-                <ul className={styles.types}>
-                  {pokemonDetail[0].types.map((t) => (
-                    <div key={pokemonDetail[0].name + t} className={t}>
-                      {t.toUpperCase()}
+                <div className={styles.infoBasica}>
+                  <div className={styles.name}>
+                    {pokemonDetail[0].name[0].toUpperCase() +
+                      pokemonDetail[0].name.slice(1)}
+                  </div>
+                  <ul className={styles.types}>
+                    {pokemonDetail[0].types.map((t) => (
+                      <div key={pokemonDetail[0].name + t} className={t}>
+                        {t.toUpperCase()}
+                      </div>
+                    ))}
+                  </ul>
+                  <div className={styles.id}>ID #{pokemonDetail[0].id}</div>
+                </div>
+              </div>
+
+              <div className={styles.contDer}>
+                <div className={styles.alturaPeso}>
+                  <div className={styles.medidas}>
+                    <div className={styles.title}>Altura</div>
+                    <div>{pokemonDetail[0].height}cm</div>
+                  </div>
+
+                  <div className={styles.medidas}>
+                    <div className={styles.title}>Peso</div>
+                    <div>{pokemonDetail[0].weight}kg</div>
+                  </div>
+                </div>
+
+                <div className={styles.stats}>
+                  <div className={styles.filaStat}>
+                    <div>Hp</div>
+                    <div className={styles.number}>{pokemonDetail[0].hp}</div>
+
+                    <div className={styles.barra}>
+                      <div
+                        className={styles.barraPintada}
+                        style={{
+                          width: `${(pokemonDetail[0].hp / 150) * 100}%`,
+                        }}
+                      ></div>
                     </div>
-                  ))}
-                </ul>
-                <div className={styles.id}>ID #{pokemonDetail[0].id}</div>
-              </div>
-            </div>
-
-            <div className={styles.contDer}>
-              <div className={styles.alturaPeso}>
-                <div className={styles.medidas}>
-                  <div className={styles.title}>Altura</div>
-                  <div>{pokemonDetail[0].height}cm</div>
-                </div>
-
-                <div className={styles.medidas}>
-                  <div className={styles.title}>Peso</div>
-                  <div>{pokemonDetail[0].weight}kg</div>
-                </div>
-              </div>
-
-              <div className={styles.stats}>
-                <div className={styles.filaStat}>
-                  <div>Hp</div>
-                  <div className={styles.number}>{pokemonDetail[0].hp}</div>
-
-                  <div className={styles.barra}>
-                    <div
-                      className={styles.barraPintada}
-                      style={{ width: `${(pokemonDetail[0].hp / 150) * 100}%` }}
-                    ></div>
                   </div>
-                </div>
 
-                <div className={styles.filaStat}>
-                  <div>Ataque</div>
-                  <div className={styles.number}>{pokemonDetail[0].attack}</div>
-                  <div className={styles.barra}>
-                    <div
-                      className={styles.barraPintada}
-                      style={{
-                        width: `${(pokemonDetail[0].attack / 150) * 100}%`,
-                      }}
-                    ></div>
+                  <div className={styles.filaStat}>
+                    <div>Ataque</div>
+                    <div className={styles.number}>
+                      {pokemonDetail[0].attack}
+                    </div>
+                    <div className={styles.barra}>
+                      <div
+                        className={styles.barraPintada}
+                        style={{
+                          width: `${(pokemonDetail[0].attack / 150) * 100}%`,
+                        }}
+                      ></div>
+                    </div>
                   </div>
-                </div>
 
-                <div className={styles.filaStat}>
-                  <div>Defensa</div>
-                  <div className={styles.number}>
-                    {pokemonDetail[0].defense}
+                  <div className={styles.filaStat}>
+                    <div>Defensa</div>
+                    <div className={styles.number}>
+                      {pokemonDetail[0].defense}
+                    </div>
+                    <div className={styles.barra}>
+                      <div
+                        className={styles.barraPintada}
+                        style={{
+                          width: `${(pokemonDetail[0].defense / 150) * 100}%`,
+                        }}
+                      ></div>
+                    </div>
                   </div>
-                  <div className={styles.barra}>
-                    <div
-                      className={styles.barraPintada}
-                      style={{
-                        width: `${(pokemonDetail[0].defense / 150) * 100}%`,
-                      }}
-                    ></div>
-                  </div>
-                </div>
 
-                <div className={styles.filaStat}>
-                  <div>Velocidad</div>
-                  <div className={styles.number}>{pokemonDetail[0].speed}</div>
-                  <div className={styles.barra}>
-                    <div
-                      className={styles.barraPintada}
-                      style={{
-                        width: `${(pokemonDetail[0].speed / 150) * 100}%`,
-                      }}
-                    ></div>
+                  <div className={styles.filaStat}>
+                    <div>Velocidad</div>
+                    <div className={styles.number}>
+                      {pokemonDetail[0].speed}
+                    </div>
+                    <div className={styles.barra}>
+                      <div
+                        className={styles.barraPintada}
+                        style={{
+                          width: `${(pokemonDetail[0].speed / 150) * 100}%`,
+                        }}
+                      ></div>
+                    </div>
                   </div>
                 </div>
               </div>
