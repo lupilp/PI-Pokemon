fbe6124abb2fef44da1f20a155836af866bec9c2
lupilp <lupilp97@gmail.com>
2022-11-01 20:42:13 -0300

    deploy fix?

--- api/src/app.js
+++ api/src/app.js
@@ -7,11 +7,9 @@ const routes = require("./routes/index.js");
 require("./db.js");
 
 const server = express();
-const cors = require("cors");
 
 server.name = "API";
 
-server.use(cors());
 server.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 server.use(bodyParser.json({ limit: "50mb" }));
 server.use(cookieParser());
@@ -30,6 +28,7 @@ server.use((req, res, next) => {
 server.use("/", routes);
 
 // Error catching endware.
+
 server.use((err, req, res, next) => {
   // eslint-disable-line no-unused-vars
   const status = err.status || 500;
