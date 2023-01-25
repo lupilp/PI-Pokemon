deb9e3c5301ca461ddebc442d0459915e9c43d40
lupilp <lupilp97@gmail.com>
2023-01-25 12:11:20 -0300

    back

--- api/index.js
+++ api/index.js
@@ -19,11 +19,10 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
-require("dotenv").config();
-const { PORT } = process.env;
+
 // Syncing all the models at once.
-conn.sync({ force: true }).then(() => {
-  server.listen(PORT, () => {
-    console.log("%s listening at", process.env.PORT); // eslint-disable-line no-console
+conn.sync({ force: false }).then(() => {
+  server.listen(process.env.PORT, () => {
+    console.log("%s listening at 3001"); // eslint-disable-line no-console
   });
 });
