ebbe07c4f53b6e26c65842876240f60cf40da4f7
lupilp <lupilp97@gmail.com>
2023-01-25 12:04:20 -0300

    changes index

--- api/index.js
+++ api/index.js
@@ -19,10 +19,11 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
-
+require("dotenv").config();
+const { PORT } = process.env;
 // Syncing all the models at once.
-conn.sync({ force: false }).then(() => {
-  server.listen(process.env.PORT, () => {
-    console.log("%s listening at 3001"); // eslint-disable-line no-console
+conn.sync({ force: true }).then(() => {
+  server.listen(PORT, () => {
+    console.log("%s listening at", process.env.PORT); // eslint-disable-line no-console
   });
 });
