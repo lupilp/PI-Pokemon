bf659562cd663296a55402c278d69d43fbd2d571
lupilp <lupilp97@gmail.com>
2023-01-25 14:37:17 -0300

    app

--- client/src/index.js
+++ client/src/index.js
@@ -9,7 +9,9 @@ import axios from "axios";
 import dotenv from "dotenv";
 dotenv.config();
 
-axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3001";
+axios.defaults.baseURL =
+  "https://pi-pokemon-production-3095.up.railway.app" ||
+  "http://localhost:3001";
 
 ReactDOM.render(
   <Provider store={store}>
