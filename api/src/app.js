1327e478a5561573c6d7f16d67790f3eaf83f9df
lupilp <lupilp97@gmail.com>
2023-01-25 14:42:13 -0300

    plis

--- api/src/app.js
+++ api/src/app.js
@@ -15,7 +15,10 @@ server.use(bodyParser.json({ limit: "50mb" }));
 server.use(cookieParser());
 server.use(morgan("dev"));
 server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://pi-pokemon-gilt.vercel.app"
+  ); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
     "Access-Control-Allow-Headers",
